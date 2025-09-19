1a411309667cbe8a84645269400309cd0ab559c5
sambhav <55543517+SambhavAthreyaGit@users.noreply.github.com>
2025-09-19 17:29:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-"""CSC111 Winter 2025: Computational Proof of F1 Driver Performance Under Distinct Constructors (Graphs Visualization)
-
-Module Description
-==================
-
-This module contains Python functions that you can use to visualize our graph.
-
-Copyright and Usage Information
-===============================
-
-This file is provided solely for the personal and private use of teachers and TAs
-in CSC111 at the University of Toronto St. George campus. All forms of
-distribution of this code, whether as given or with any changes, are
-expressly prohibited.
-
-This file is Copyright (c) 2025 Pranay Chopra, Sambhav Athreya, Sumedh Gadepalli, and Firas Adnan Jalil.
-"""
-
 import dash
 from dash import dcc, html, Input, Output, State, dash_table
 import dash_cytoscape as cyto
